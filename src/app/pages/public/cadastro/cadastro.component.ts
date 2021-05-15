8ce9dcb700dbb25da1b1e219a33b6d3dfe7aadd4
Luciano Brito <lucianobrito.dev@gmail.com>
2021-05-15 14:07:40 -0300

    Update cadastro.component.ts

--- src/app/pages/public/cadastro/cadastro.component.ts
+++ src/app/pages/public/cadastro/cadastro.component.ts
@@ -1,100 +1,148 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
-import { AlunoService } from 'src/app/services/aluno.service'
-import { ProfessorService } from 'src/app/services/professor.service';
-import { Usuario } from 'src/app/models/usuario';
+import { Usuario } from "src/app/models/usuario";
+import { AlunoService } from "src/app/services/aluno.service";
+import { ProfessorService } from "src/app/services/professor.service";
+import { CursoService } from "src/app/services/curso.service";
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
 })
-
 export class CadastroComponent implements OnInit {
-
   checkRadio: boolean = true;
-  placeholder: string = 'Digite o curso que leciona';
+  placeholder: string = "Digite o curso que leciona";
   user: Usuario = {};
-  textButton = 'Cadastrar';
-  typeUser = 'Professor';
+  textButton = "Cadastrar";
+  typeUser = "Professor";
   colspan = 2;
 
-
   constructor(
-      private router: Router,
-      private professorService: ProfessorService,
-      private alunoService: AlunoService
-    ) { }
+    private router: Router,
+    private professorService: ProfessorService,
+    private alunoService: AlunoService,
+    private cursoService: CursoService
+  ) {}
 
-  ngOnInit(): void {
-    }
-  }
+  ngOnInit(): void {}
 
   radioCheck(): void {
-    if(this.checkRadio == false) {
-      this.placeholder = 'Digite o curso que deseja ser matriculado';
-      this.typeUser = 'Aluno';
+    if (this.checkRadio == false) {
+      this.placeholder = "Digite o curso que deseja ser matriculado";
+      this.typeUser = "Aluno";
     } else {
-      this.placeholder = 'Digite o curso que leciona';
-      this.typeUser = 'Professor';
+      this.placeholder = "Digite o curso que leciona";
+      this.typeUser = "Professor";
     }
   }
 
-  cadastrar(user) {
-    if (user.tipo == 1) {
-      if (this.textButton === 'Cadastrar') {
-        this.professorService.incluir(user).subscribe(
-          success => {
-            console.log(`Professor ${user.nome} incluído com sucesso!`)
-            this.navegar('');
-          },
-          error => {
-            console.log(`Não foi possível incluir o professor ${user.nome}, erro ocorrido!`)
-            this.navegar('');
-          },
-          () => console.log('Requisição completa!'));
-      } else {
-        /* this.editar(user); */
-      }
-    } else {
-      if (this.textButton === 'Cadastrar') {
-        this.alunoService.incluir(user).subscribe(
-          success => {
-            console.log(`Aluno ${user.nome} incluído com sucesso!`)
-            this.navegar('');
-          },
-          error => {
-            console.log(`Não foi possível incluir o aluno ${user.nome}, erro ocorrido!`)
-            this.navegar('');
-          },
-          () => console.log('Requisição completa!'));
-      } else {
-        /* this.editar(user); */
-      }
+  cadastrar(dado) {
+    switch (dado.tipo) {
+      case '1':
+        this.textButton === "Cadastrar"
+          ? this.cadastrarProfessor(dado)
+          : this.editarProfessor(dado);
+        break;
+
+      case '2':
+        this.textButton === "Cadastrar"
+          ? this.cadastrarAluno(dado)
+          : this.editarAluno(dado);
+        break;
+
+      case '3':
+        this.textButton === "Cadastrar"
+          ? this.cadastrarCurso(dado)
+          : this.editarCurso(dado);
+        break;
+
+      case '4':
+        this.textButton === "Cadastrar"
+          ? this.cadastrarAula(dado)
+          : this.editarAula(dado);
+        break;
     }
   }
 
-  cadastrarAluno() {  }
+  cadastrarProfessor(user) {
+    this.professorService.incluir(user).subscribe(
+      (success) => {
+        this.successMessage(user, 'incluir');
+      },
+      (error) => {
+        this.errorMessage(user, 'incluir');
+      },
+      () => console.log("Requisição completa!")
+    );
+  }
 
-  cadastrarProfessor() {  }
+  cadastrarAluno(user) {
+    this.alunoService.incluir(user).subscribe(
+      (success) => {
+        this.successMessage(user, 'incluir');
+      },
+      (error) => {
+        this.errorMessage(user, 'incluir');
+      },
+      () => console.log("Requisição completa!")
+    );
+  }
 
-  cadastrarCurso() {  }
+  cadastrarCurso(curso) {
+    this.cursoService.incluir(curso).subscribe(
+      (success) => {
+        this.successMessage(curso, 'incluir');
+      },
+      (error) => {
+        this.successMessage(curso, 'incluir');
+      },
+      () => console.log("Requisição completa!")
+    );
+  }
 
-  cadastrarAula() {  }
+  cadastrarAula(aula) {}
+
+  editarProfessor(user) {}
+
+  editarAluno(user) {}
+
+  editarCurso(curso) {}
+
+  editarAula(aula) {}
+
+  successMessage(param, oper) {
+    if (oper === 'incluir') {
+      console.log(`${this.typeUser} ${param.nome} incluído com sucesso!`);
+      this.cancelar();
+    } else {
+      console.log(`${this.typeUser} ${param.nome} alterado com sucesso!`);
+      this.cancelar();
+    }
+  }
+
+  errorMessage(param, oper) {
+    if (oper === 'incluir') {
+      console.log(`Não foi possível incluir o ${this.typeUser} ${param.nome}, erro ocorrido!`);
+      this.cancelar();
+    } else {
+      console.log(`Não foi possível incluir o ${this.typeUser} ${param.nome}, erro ocorrido!`);
+      this.cancelar();
+    }
+  }
 
   limpar() {
     this.user = {};
-    this.typeUser = 'Professor';
+    this.typeUser = "Professor";
   }
 
-  cancelar(){
+  cancelar() {
     this.limpar();
-    this.navegar('');
+    this.navegar("");
   }
 
   navegar = (route: any) => {
     this.router.navigate([route]);
-  }
-
+  };
 }
